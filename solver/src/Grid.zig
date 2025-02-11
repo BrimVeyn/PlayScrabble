1ff6abb1a60771cc1efb2a43765f18d841c3da04
Bryan VAN PAEMEL <bryan.vanpaemel@gmail.com>
2025-02-11 19:20:16 +0000

    Add(Grid-solver)     - Grid.zig : populated loadGridState that lets you load a txt file       that contains a grid state     - main.zig : refactored, added time measurments. Started       implementing solveGrid function.

--- solver/src/Grid.zig
+++ solver/src/Grid.zig
@@ -1,3 +1,8 @@
+const std           = @import("std");
+const gridRootPath  = "./src/testGrids/";
+const MAX_PATH      = 256;
+const GRID_SIZE     = 15 * 16;
+
 pub const Grid = struct {
 
     const Modifiers = enum(u8) {
@@ -8,14 +13,25 @@ pub const Grid = struct {
         NONE,
     };
     
-    modifiers: [15][15]Modifiers,
-    grid: [15][15]u8,
+    modifiers:  [15][15]Modifiers,
+    grid:       [15][15]u8,
 
+    pub fn loadGridState(self: *Grid, path: []const u8) !void {
+        var pathBuffer: [MAX_PATH:0]u8 = .{0} ** MAX_PATH;
+        std.mem.copyForwards(u8, pathBuffer[0..], gridRootPath);
+        std.mem.copyForwards(u8, pathBuffer[gridRootPath.len..], path);
 
-    pub fn loadGridState(self: *Grid, path: []const u8) void {
-        _ = self;
-        _ = path;
-        return ;
+        const cwd = try std.fs.cwd().openFileZ(pathBuffer[0..], .{});
+        defer cwd.close();
+
+        var gridBuffer: [GRID_SIZE:0]u8 = .{0} ** GRID_SIZE;
+        _ = try cwd.reader().readAll(&gridBuffer);
+
+        var lineIt = std.mem.tokenizeScalar(u8, gridBuffer[0..], '\n');
+        var y: usize = 0;
+        while (lineIt.next()) |line| : (y += 1) {
+            std.mem.copyForwards(u8, self.grid[y][0..], line);
+        }
     }
 
     pub fn init() Grid {
--- solver/src/main.zig
+++ solver/src/main.zig
@@ -1,34 +1,33 @@
-const std               = @import("std");
-const dict              = @embedFile("generate/Data.txt");
-const print             = std.debug.print;
-const AutoArrayHashMap  = std.AutoArrayHashMap;
-const AutoHashMap       = std.AutoHashMap;
-const ArrayList         = std.ArrayList;
-const Allocator         = std.mem.Allocator;
+const std = @import("std");
+const dict = @embedFile("generate/Data.txt");
+const print = std.debug.print;
+const AutoArrayHashMap = std.AutoArrayHashMap;
+const AutoHashMap = std.AutoHashMap;
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
 
-const gridFile          = @import("Grid.zig");
-const Grid              = gridFile.Grid;
+const gridFile = @import("Grid.zig");
+const Grid = gridFile.Grid;
 
-const generator             = @import("generate/generate.zig");
-const OrderedMap            = generator.OrderedMap;
-const asciiOrderedMapPath   = generator.asciiOrderedMapPath;
-const Map                   = generator.Map;
+const generator = @import("generate/generate.zig");
+const OrderedMap = generator.OrderedMap;
+const asciiOrderedMapPath = generator.asciiOrderedMapPath;
+const Map = generator.Map;
 
-const PermList  = std.StringArrayHashMap(bool);
-const String    = ArrayList(u8);
+const PermSet = std.StringArrayHashMap(bool);
+const String = ArrayList(u8);
+const StringVec = ArrayList([]const u8);
 
-pub fn permutations(alloc: Allocator, perms: *PermList, pattern: []u8, buffer: *String, patternI: usize, minLen: usize, maxLen: usize) !void {
+pub fn permutations(alloc: Allocator, perms: *PermSet, pattern: *String, buffer: *String, patternI: usize, minLen: usize, maxLen: usize) !void {
     if (buffer.items.len != 0) {
-        if (buffer.items.len >= minLen and buffer.items.len <= maxLen 
-            and !perms.contains(buffer.items)) 
-        {
+        if (buffer.items.len >= minLen and buffer.items.len <= maxLen and !perms.contains(buffer.items)) {
             try perms.put(try alloc.dupe(u8, buffer.items), true);
         }
     }
-    if (patternI >= pattern.len)
+    if (patternI >= pattern.items.len)
         return;
 
-    try buffer.append(pattern[patternI]);
+    try buffer.append(pattern.items[patternI]);
     try permutations(alloc, perms, pattern, buffer, patternI + 1, minLen, maxLen);
 
     _ = buffer.pop();
@@ -50,50 +49,88 @@ fn lessThan(context: void, a: u8, b: u8) bool {
     return (a < b);
 }
 
+fn getWordList(alloc: Allocator, perms: *PermSet, orderedMap: Map) !StringVec {
+    var vec = StringVec.init(alloc);
+
+    for (perms.keys()) |permutation| {
+        const permWords = orderedMap.data.get(permutation) orelse continue;
+        for (permWords.keys()) |word| {
+            try vec.append(word);
+        }
+    }
+    return vec;
+}
+
+const Match = struct {
+    word: [15]u8,
+};
+const MatchVec = ArrayList(Match);
+
+fn evaluateGrid(alloc: Allocator, grid: *Grid, orderedMap: Map) !MatchVec {
+    const result = MatchVec.init(alloc);
+    _ = grid;
+    _ = orderedMap;
+
+    return result;
+}
+
+fn solveGrid(alloc: Allocator, grid: *Grid, rack: *String, orderedMap: Map) !void {
+    const startTime = std.time.microTimestamp();
+
+    var buffer = String.init(alloc);
+    defer buffer.deinit();
+
+    var perms = PermSet.init(alloc);
+    defer {
+        for (perms.keys()) |key| alloc.free(key);
+        perms.deinit();
+    }
+
+    try permutations(alloc, &perms, rack, &buffer, 0, 2, rack.items.len);
+    // for (perms.keys(), 0..) |word, i| {
+    //     print("vect[{d}] = {s}\n", .{i, word});
+    // }
+    // print("Count: {d}\n", .{perms.keys().len});
+    const wordVec = try getWordList(alloc, &perms, orderedMap);
+    defer wordVec.deinit();
+
+    const resultFirstHalf = try evaluateGrid(alloc, grid, orderedMap);
+    _ = resultFirstHalf;
+    // for (wordVec.items, 0..) |word, i| {
+    //     print("vect[{d}] = {s}\n", .{i, word});
+    // }
+    // print("Count: {d}\n", .{perms.keys().len});
+
+    const endTime = std.time.microTimestamp();
+    const elapsedMicro: i64 = endTime - startTime;
+    const elapsedMili: f64 = @as(f64, @floatFromInt(elapsedMicro)) / @as(f64, 1000);
+
+    print("Elapsed: {d}µs | {d}ms\n", .{elapsedMicro, elapsedMili});
+}
+
 pub fn main() !void {
     var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
     const GpaAlloc = gpa.allocator();
-    defer {
-        const leaks = gpa.deinit();
-        _ = leaks;
-    }
+    defer _ = gpa.deinit();
 
     var arena = std.heap.ArenaAllocator.init(GpaAlloc);
     const ArenaAlloc = arena.allocator();
     defer arena.deinit();
 
+    //Representation of the game state
     var grid = Grid.init();
-    grid.loadGridState("salope");
+    try grid.loadGridState("grid00.txt");
 
+    //Representation of the players'rack
+    var rack = String.init(ArenaAlloc);
+    try rack.appendSlice("EVENTUELLEMENTE");
+    std.mem.sort(u8, rack.items[0..], {}, lessThan);
+
+    //Dictionnairies with sorted ascii keys to array of possible words
     var map: Map = try populateMap(ArenaAlloc);
     defer map.deinit(ArenaAlloc);
 
-
-    // print("{}\n", .{map});
-
-    var pattern = [_]u8 {'A', 'M', 'U', 'S', 'E'};
-    std.mem.sort(u8, pattern[0..], {}, lessThan);
-
-    print("Pattern: {s}\n", .{pattern});
-
-    var buffer = String.init(ArenaAlloc);
-    defer buffer.deinit();
-    
-    var perms = PermList.init(ArenaAlloc);
-    defer {
-        for (perms.keys()) |key| ArenaAlloc.free(key);
-        perms.deinit();
-    }
-
-    try permutations(ArenaAlloc, &perms, pattern[0..], &buffer, 0, 2, pattern.len);
-    for (perms.keys()) |key| {
-        print("Key: {s}\n", .{key});
-    }
+    try solveGrid(ArenaAlloc, &grid, &rack, map);
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+test "simple test" {}
