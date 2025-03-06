6b75356ffce2610d1158c44e90ae0ee9a379ca4c
Bryan VAN PAEMEL <bryan.vanpaemel@gmail.com>
2025-03-06 06:28:33 +0000

    Add(frontend) Working on language selection

--- frontend/src/navbar/Navbar.tsx
+++ frontend/src/navbar/Navbar.tsx
@@ -2,34 +2,51 @@ import { useTranslation } from "react-i18next";
 import { useState } from "react";
 import "./Navbar.css"
 
-//function loginButton () {
-//	const [loginShown, setLoginShown] = useState<boolean>(false);
-//
-//	useEffect(() => {
-//		if (!loginShown) return;
-//	}, [loginShown]);
-//
-//
-//	const showLoginModal = () => {
-//		setLoginShown((prev: boolean) => !prev);
-//	}
-//
-//	return (
-//		<button onClick={() => showLoginModal()} className="loginButton">Se connecter</button>
-//	)
-//}
-
-
 const languages: { [id: string] : string; } = {
 	fr: "🇫🇷",
 	en: "🇬🇧",
 };
 
+function LanguageDropdown() {
+	const langKeys = Object.keys(languages);
+	const {i18n} = useTranslation();
+	const [selected, setSelected] = useState<string | null>(i18n.language);
+
+	const handleClick = (lang: string) => {
+		if (selected !== lang) {
+			i18n.changeLanguage(lang)
+			setSelected(lang);
+		}
+	}
+
+	return (
+		<div className="langDropContainer">
+			<div className="langDropItems">
+				{ langKeys.map((lang: string) => (
+					<div 
+						className="langDropItem"
+						key={lang}
+						onClick={() => handleClick(lang)}
+					>
+						<input 
+							type="radio" 
+							id={lang}
+							checked={lang === selected}
+						/>
+						<p> {lang.toUpperCase()} </p>
+						<p> {languages[lang]} </p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
+
 function Navbar() {
-	const { t, i18n } = useTranslation("navbar");
+	const { t } = useTranslation("navbar");
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 	
-	const langKeys = Object.keys(languages);
 
 	return (
 		<>
@@ -48,18 +65,7 @@ function Navbar() {
 			</div>
 		</div>
 		{ isOpen && (
-			<div className="langDropContainer">
-				<div className="langDropItems">
-				{ langKeys.map((lang: string) => (
-					<p 
-						key={lang}
-						onClick={() => i18n.changeLanguage(lang)}
-					> 
-						{lang.toUpperCase()} - {languages[lang]}
-					</p>
-				))}
-				</div>
-			</div>
+			<LanguageDropdown/>
 		)}
 		</>
 	);
